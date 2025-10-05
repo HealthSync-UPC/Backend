e950773eb56b43a31f560e5d2f05a4a39e4da8d2
Franco Durand <francoduve.25@gmail.com>
2025-10-05 16:46:18 -0500

    first commit

---
+++ src/test/java/pe/healthsync/meditrack/MeditrackApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.healthsync.meditrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeditrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
