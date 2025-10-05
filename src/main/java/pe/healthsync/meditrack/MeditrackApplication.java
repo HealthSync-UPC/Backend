3d4d22ef3458c9a1e9907ede590975660ea8c431
Franco Durand <francoduve.25@gmail.com>
2025-10-05 17:09:38 -0500

    feat: add initial application configuration and auditing support

--- src/main/java/pe/healthsync/meditrack/MeditrackApplication.java
+++ src/main/java/pe/healthsync/meditrack/MeditrackApplication.java
@@ -2,8 +2,10 @@ package pe.healthsync.meditrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MeditrackApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/pe/healthsync/meditrack/shared/domain/model/entities/AuditableModel.java
@@ -0,0 +1,24 @@
+package pe.healthsync.meditrack.shared.domain.model.entities;
+
+import java.time.Instant;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class AuditableModel {
+    @Getter
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Getter
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Instant updatedAt;
+}
